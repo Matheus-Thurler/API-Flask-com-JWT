8ad3437ee73de7c825f1aaa63201f8428267af47
Matheus Thurler <matheusthurlernf@gmail.com>
2021-01-13 11:25:37 -0300

    remoção da pasta routes temporariamente

--- src/controllers/listHoteisController.py
+++ src/controllers/listHoteisController.py
@@ -5,10 +5,3 @@ from src.db.hotel import hoteis
 class ListHoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
-
-class Hotel(Resource):
-    def get(self, hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return {'message': 'Hotel not Found.'}, 404
--- src/routes/hotelRoute.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Api
-from app import app
-
-api = Api(app)
-
-api.add_resource(Hoteis, '/hoteis')

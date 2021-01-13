32bbb9e7b495ce60a547b2da1dcb1d8b352a2968
Matheus Thurler <matheusthurlernf@gmail.com>
2021-01-13 17:24:43 -0300

    criação da coneção do mysql e modelo de banco

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_restful import Api
 
+
 from src.controllers.listHoteisController import ListHoteis
 from src.controllers.HotelController import Hotel
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Redqueen@localhost/hoteis'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 api = Api(app)
+
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 
 api.add_resource(ListHoteis, '/hoteis')
@@ -14,4 +24,6 @@ api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 
 if __name__ == "__main__":
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
